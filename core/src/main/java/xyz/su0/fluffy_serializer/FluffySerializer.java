c3fb670b81ceaa303a8d698a8ec6bb49dc00cb98
kirillsulim <kirillsulim@gmail.com>
2015-07-03 13:52:51 +0500

    So it begins =)

---
+++ core/src/main/java/xyz/su0/fluffy_serializer/FluffySerializer.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+package xyz.su0.fluffy_serializer;
+
+public class FluffySerializer {
+  public String serialize(Object object) {
+    return "Daaa";
+  }
+
+  public Object deserialize(String string) {
+    return null;    
+  }
+}
---
+++ core/src/test/java/xyz/su0/fluffy_serializer/FluffySerializerTest.java
@@ -0,0 +1,24 @@
+/*
+ *
+ */
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+import xyz.su0.fluffy_serializer.FluffySerializer;
+
+public class FluffySerializerTest {
+  FluffySerializer sz;
+
+  @Before
+  public void setUp() {
+    sz = new FluffySerializer();
+  }
+
+  @Test
+  public void shouldSerializeString() {
+    String serializedString = sz.serialize("Some string");
+    assertEquals(serializedString, "\"Some String\"");
+  }
+}
